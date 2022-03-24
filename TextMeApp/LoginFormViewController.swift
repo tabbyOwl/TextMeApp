320def962f93456c5cdd817d8db2d2590d2bce5d
Светлана <spo_17@mail.ru>
2022-03-24 19:09:18 +0800

    lesson2

--- TextMeApp/LoginFormViewController.swift
+++ TextMeApp/LoginFormViewController.swift
@@ -14,15 +14,44 @@ class LoginFormViewController: UIViewController {
     
     @IBOutlet weak var passwordInput: UITextField!
     
-    @IBAction func loginButtonPressed(_ sender: Any) {
-        guard let login = loginInput.text else { return }
-        guard let password = passwordInput.text else { return }
-        if login == "admin" && password == "123456" {
-            print("успешная авторизация")
-        } else {
-            print("неуспешная авторизация")
-        }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+    // Проверяем данные
+    let checkResult = checkUserData()
+    // Если данные не верны, покажем ошибку
+    if !checkResult { showLoginError()
     }
+    // Вернем результат
+    return checkResult
+    }
+    
+    func checkUserData() -> Bool {
+    guard let login = loginInput.text,
+    let password = passwordInput.text else { return false }
+    if login == "" && password == "" { return true
+    } else {
+    return false
+    }
+    }
+    
+    func showLoginError() { // Создаем контроллер
+    let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+    // Создаем кнопку для UIAlertController
+    let action = UIAlertAction(title: "OK", style: .cancel, handler: nil) // Добавляем кнопку на UIAlertController
+    alter.addAction(action)
+    // Показываем UIAlertController
+    present(alter, animated: true, completion: nil)
+        
+    }
+    
+//    @IBAction func loginButtonPressed(_ sender: Any) {
+//        guard let login = loginInput.text else { return }
+//        guard let password = passwordInput.text else { return }
+//        if login == "admin" && password == "123456" {
+//            print("успешная авторизация")
+//        } else {
+//            print("неуспешная авторизация")
+//        }
+//    }
     
     override func viewDidLoad() {
         
